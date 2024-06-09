ac74cb45e5c5625dc108da803164e77f089b5435
Anh Tran <anh.thi.tram.tran@gmail.com>
2024-06-08 21:48:33 -0500

    Fixing the project section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -13,9 +13,9 @@ const Tech = () => {
         <p className={styles.sectionSubText}>I know to work with...</p>
         <h2 className={styles.sectionHeadText}>Skills</h2>
       </motion.div>
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
+      <div className='flex flex-row flex-wrap justify-center gap-10 pt-10'>
         {technologies.map((technology) => (
-          <div className='w-28 h-28' key={technology.name}>
+          <div className='w-25 h-25' key={technology.name}>
             <div className="bg-black-200 w-32 h-32 p-6 rounded-full"> {/* Background styling */}
               <img
                 src={technology.icon}
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import {Tilt} from "react-tilt";
-import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({
-  index,
   name,
   description,
   tags,
@@ -17,14 +14,14 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <div
+    <div>
+      <Tilt
         options={{
           max: 45,
           scale: 1,
-          speed: 500,
+          speed: 450,
         }}
-        className='bg-black-100 p-5 rounded-2xl sm:w-[320px] w-full'
+        className='bg-black-100 p-5 rounded-2xl sm:w-[360px] w-full'
       >
         <div className='relative w-full h-[230px]'>
           <img
@@ -62,33 +59,28 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
-      </div>
-    </motion.div>
+      </Tilt>
+    </div>
   );
 };
 
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>My work</p>
-        <h2 className={styles.sectionHeadText}>Projects</h2>
-      </motion.div>
-
-      <div className='w-full flex'>
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
-        >
-          The following projects demonstrate my abilities and expertise by 
-          presenting real-world projects. Each project is concisely 
-          described and includes links to the code repositories. These projects 
-          showcase my capacity to tackle complex problems, utilize diverse 
-          technologies, and efficiently manage them.
-        </motion.p>
+      <div>
+        <p className={`${styles.sectionSubText} `}>My work</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </div>
 
-      <div className='mt-20 flex flex-wrap gap-12 justify-center'>
+      <div className='w-full flex'>
+        <p
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          The following projects demonstrate my abilities and expertise by presenting real-world projects. Each project is concisely described and includes links to the code repositories. These projects showcase my capacity to tackle complex problems, utilize diverse technologies, and efficiently manage them.
+        </p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-7'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
